2a4bb1622adf2fa8bb6a65507697677b6a1a9a9e
Asmau Yusuf Abdullahi <asmauabdullahi19@gmail.com>
2026-07-23 11:50:07 +0100

    input enter key update

--- vite-project/src/components/TaskInput.jsx
+++ vite-project/src/components/TaskInput.jsx
@@ -11,12 +11,16 @@ const TaskInput = ({onAdd}) => {
         setText('')
     }
 
+    const handleKey = (e) => {
+        if (e.key === 'Enter') handleAdd()
+    }
+
   return (
     <>
         <div className='task-input-row'>
             <input type="text" 
             value={text} 
-            onChange={(e) => setText(e.target.value)} placeholder='Add a new task...' />
+            onChange={(e) => setText(e.target.value)} onKeyDown={handleKey} placeholder='Add a new task...' />
             <button onClick={handleAdd}>Add task</button>
         </div>
     </>
